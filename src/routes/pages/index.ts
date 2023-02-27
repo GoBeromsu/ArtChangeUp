1878472c95acae2f7413a287befee213210bce25
goberomsu <310o@naver.com>
2023-02-27 14:02:57 +0900

    Fix: 뷰 수정

---
+++ src/routes/pages/index.ts
@@ -0,0 +1,8 @@
+import {FastifyReply} from "fastify";
+import fastify, {FastifyInstance, FastifyRequest} from "fastify";
+
+export default async function (fastify: FastifyInstance) {
+  fastify.get("/", (req: FastifyRequest, reply: FastifyReply) => {
+    reply.view("views/index.ejs")
+  });
+}
