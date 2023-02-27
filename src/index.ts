b78d725edab23f8c808ee0bbdb081e63fd25301b
goberomsu <310o@naver.com>
2023-02-27 14:02:25 +0900

    Fix: socket.io 의존성 문제 해결

--- src/index.ts
+++ src/index.ts
@@ -1,12 +1,13 @@
 import Fastify, {FastifyInstance} from "fastify";
 import {Server, IncomingMessage, ServerResponse} from "http";
 import routes from "@routes/index";
+import middleware from "@routes/middleware/loader";
 
 const PORT: number = Number(process.env.PORT) || 4000;
 const fastify: FastifyInstance<Server, IncomingMessage, ServerResponse> = Fastify();
 
+middleware(fastify);
 routes(fastify);
-
 async function start() {
   try {
     await fastify.listen({port: PORT, host: "0.0.0.0"});
