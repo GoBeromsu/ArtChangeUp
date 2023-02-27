1e94562b8c3fb07aa555c460fd13eb54bdbee5f0
goberomsu <310o@naver.com>
2023-02-27 10:43:21 +0900

    Fix : Loading Error 수정

--- src/index.ts
+++ src/index.ts
@@ -1,12 +1,15 @@
 import Fastify, {FastifyInstance} from "fastify";
 import {Server, IncomingMessage, ServerResponse} from "http";
+import routes from "@routes/index";
 
 const PORT: number = Number(process.env.PORT) || 4000;
 const fastify: FastifyInstance<Server, IncomingMessage, ServerResponse> = Fastify();
 
-function start() {
+routes(fastify);
+
+async function start() {
   try {
-    fastify.listen({port: PORT, host: "0.0.0.0"});
+    await fastify.listen({port: PORT, host: "0.0.0.0"});
     console.log(`server start! http://127.0.0.1:${PORT}/`);
   } catch (err: any) {
     // logger.error(`server loading error... ${err}`);
